ab9dae37b9e71584f4d7ca54e8665407d98939ae
Digiprog1 <adebiyiolasunkanmi@gmail.com>
2024-03-12 17:18:37 +0100

    yelp uploaded to git

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "yelpamplif1238af70937": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
---
+++ src/App.js
@@ -0,0 +1,30 @@
+import { withAuthenticator } from 'aws-amplify-react';
+import logo from './logo.svg';
+import './App.css';
+import Amplify from 'aws-amplify';
+import awsConfig from './aws-exports';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+
+Amplify.configure(awsConfig);
+export default withAuthenticator (App);
